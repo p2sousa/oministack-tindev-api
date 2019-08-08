7922d9c5fc96de0b30526dea7648b1d551de7569
Pablo Sousa <pablosousa.ads@gmail.com>
2019-08-08 19:41:04 -0300

    modificado model Dev

--- src/routes/devs/index.js
+++ src/routes/devs/index.js
@@ -5,7 +5,7 @@ import DislikesController from '../../controllers/DislikesController';
 export default (router) => {
   router.get('/devs', async (req, res) => {
     const DevController = new DevsController();
-    const response = await DevController.index();
+    const response = await DevController.index(req);
 
     res.status(response.statusCode);
     res.json(response.data);
@@ -13,7 +13,7 @@ export default (router) => {
 
   router.post('/devs', async (req, res) => {
     const DevController = new DevsController();
-    const response = await DevController.create(req.body);
+    const response = await DevController.create(req);
 
     res.status(response.statusCode);
     res.json(response.data);
@@ -21,7 +21,7 @@ export default (router) => {
 
   router.post('/devs/:devId/likes', async (req, res) => {
     const LikeController = new LikesController();
-    const response = await LikeController.create(req.body);
+    const response = await LikeController.create(req);
 
     res.status(response.statusCode);
     res.json(response.data);
@@ -29,7 +29,7 @@ export default (router) => {
 
   router.post('/devs/:devId/dislikes', async (req, res) => {
     const DislikeController = new DislikesController();
-    const response = await DislikeController.create(req.body);
+    const response = await DislikeController.create(req);
 
     res.status(response.statusCode);
     res.json(response.data);
