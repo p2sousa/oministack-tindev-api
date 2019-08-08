661f78ab3900146af592fc77c92d749cc7130223
Pablo Sousa <pablosousa.ads@gmail.com>
2019-08-08 18:28:50 -0300

    corrgido pacotes do babel para executar os testes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 import routes from './src/routes/index';
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/omnistack?retryWrites=true', { useNewUrlParser: true });
 
 app.use(express.json());
 app.use(cors());
